948767ba98bf5304ca995146f840526d7c06fac9
wangyi <haveaballmyball@hotmail.com>
2021-03-12 11:12:46 +0800

    feat:添加项目文件

---
+++ src/views/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    这是CSS列表页
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
